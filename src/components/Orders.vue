ee437bb0bc249be6773e95fba0ea2b124783f0b1
赵志勇 <648561983@qq.com>
2021-03-18 19:53:34 +0800

    完成了订单管理功能

--- src/components/Orders.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h3>订单列表</h3>
-</template>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -39,6 +39,8 @@ import {
   Checkbox,
   Upload
 } from 'element-ui'
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -75,6 +77,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 // 把弹窗组件挂载到vue上，这样每个组件都可以通过this访问到$message
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
